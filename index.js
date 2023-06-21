5c33726b742462d4e3cf605ac72c639211833390
NISHANT-RATHOD <nishantrathod555@gmail.com>
2023-06-21 22:32:46 +0530

    first commit

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app  = express();
+const DB = process.env.DATABASE
+const PORT  = process.env.PORT || 5000
 app.use(express.json());
 app.use(cors());
 app.get("/test",(req,res)=>{
@@ -22,8 +24,6 @@ app.use("/shop/product",ProductRouter);
 app.use("/shop/cart",CartRouter);
 app.use("/shop/order",OrderRouter);
 
-const DB = process.env.DATABASE  
-
 
 mongoose.connect(DB)
   .then(() =>{
@@ -32,6 +32,6 @@ mongoose.connect(DB)
       console.log("mongodb connection failed.");
   })
 
-app.listen(5000,()=>{
+app.listen(PORT,()=>{
     console.log("backend Server is Running");
 })
